b8cd6e50357d895ae39ed68614153630bf1f9455
nizamuddinquader <nizamuddinquader@gmail.com>
2026-06-17 01:06:06 +0600

    nothing

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string(('phone'))->nullable();
             $table->rememberToken();
             $table->timestamps();
+            
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
